cfcc6a0191f388ef7269457797b1ef2a14d10cce
santhosh11dec <santhosh11dec@gmail.com>
2015-09-26 19:45:15 +0530

    Update ESCcodeThrusters.py

--- ESCcodeThrusters.py
+++ ESCcodeThrusters.py
@@ -1,25 +1,25 @@
 from Tkinter import * 
-#import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO 
 import time
 
 GPIO.setmode(GPIO.BCM) 
-GPIO.setup(18, GPIO.OUT) 
-pwm = GPIO.PWM(18, 100)
-pwm.start(5)
+GPIO.setup(14, GPIO.OUT) 
+pwm = GPIO.PWM(14, 100) #100Hz - 0.01s or 10 ms signals
+pwm.start(15)
 time.sleep(2)
 
 class App: 
     def __init__(self, master): 
         frame = Frame(master) 
         frame.pack()
-        scale = Scale(frame, from_=1100, to=1900, orient=HORIZONTAL, command=self.update, resolution = 100) 
+        scale = Scale(frame, from_=1100, to=1900, orient=HORIZONTAL, command=self.update) 
         scale.grid(row=0) 
-        angle = 1500
+        angle = 1500 #1500us pulse in signal represents a stop signal
     def update(self, angle): 
-        duty = (((float(angle)/1000)/10)*100)
+        duty = (((float(angle)/1000)/10)*100) #conversion to duty cycle - 1.5ms pulse in 10ms signals
         pwm.ChangeDutyCycle(duty) 
         print duty
-        
+    
 root = Tk()
 root.wm_title('Servo Control')
 app = App(root)
